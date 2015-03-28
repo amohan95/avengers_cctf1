2a88efd59983fe7bde5d797fabda466562905a58
Ananth Mohan <ananthmo@usc.edu>
2015-03-28 14:50:13 -0700

    Fixed commandline argument parsing in monitor scripts

--- blue_defence/white_monitor.sh
+++ blue_defence/white_monitor.sh
@@ -1,9 +1,9 @@
 if [[ ! -n $1 || ! -n $2 ]]; then
   echo "Usage is ./white_monitor.sh <servers_file> <ip_map_file>"
 else
+  servers_file=$1
+  ip_map_file=$2
   while true; do
-    servers_file=$1
-    ip_map_file=$2
     ./ip_map_creator.sh "${servers_file}" "${WCACHE_IP}" _"${ip_map_file}"
     diff --unchanged-line-format= --old-line-format= --new-line-format='%L' "${ip_map_file}" _"${ip_map_file}" > _blue_diffs.txt
     while read line; do
