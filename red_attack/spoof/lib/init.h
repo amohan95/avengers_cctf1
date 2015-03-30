441e697fde872b8ed9b78b8996e82a5262938e06
Will Alexander <github.com@willalexander.me>
2015-03-30 14:22:09 -0700

    Add sock creation code

---
+++ red_attack/spoof/lib/init.h
@@ -0,0 +1,4 @@
+#include <arpa/inet.h>
+
+int si_init(struct sockaddr_in *si);
+int sock_init();
--- red_attack/spoof/spoof.c
+++ red_attack/spoof/spoof.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include <stdlib.h>     // exit
+#include <stdio.h>      // fprintf, perror
+#include <unistd.h>     // close
+#include "init.h"       // si_init, sock_init
 
 int main()
 {
+	int s = sock_init();
+	struct sockaddr_in si = { 0 };
+	int si_init_ret = si_init(&si);
+	if (si_init_ret == 0) {
+		fprintf(stderr, "si_init: invalid address\n");
+		exit(1);
+	} else if (si_init_ret == -1) {
+		perror("si_init");
+		exit(1);
+	}
+	if (close(s)) {
+		perror("close");
+		exit(1);
+	}
 	return 0;
 }
