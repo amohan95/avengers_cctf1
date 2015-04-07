80a0e1ba377e383c69b5bb2456a33c0795c8d5e0
Will Alexander <github.com@willalexander.me>
2015-04-07 05:32:35 -0700

    Add question serialization

--- red_attack/spoof/dns_test.c
+++ red_attack/spoof/dns_test.c
@@ -1,13 +1,34 @@
-#include <assert.h>
+#include <stdio.h>
 #include "dns.h"
 
-void test_sizeof()
-{
-	assert(sizeof(struct dns_hdr) == sizeof(uint16_t) * 6);
-}
+#define BCACHE_HOSTNAME "bcache"
+#define WCACHE_HOSTNAME "wcache"
+/* Change this to the name of the experiment you're running */
+#define TESTBED_HOSTNAME "cctf1-will"
+#define TARGET_HOSTNAME WCACHE_HOSTNAME
 
 int main()
 {
-	test_sizeof();
+	char *name[] = {
+		TARGET_HOSTNAME,
+		TESTBED_HOSTNAME,
+		"USC430",
+		"isi",
+		"deterlab",
+		"net",
+	};
+
+	size_t len_name = sizeof name / sizeof *name;
+	size_t len = len_dns_q(name, len_name);
+	uint8_t q_data[len];
+
+	if (build_dns_q(q_data, 1, name, len_name) != 0) {
+		perror("build_dns_q");
+	}
+
+	for (int i = 0; i < len; ++i) {
+		printf("%i\n", q_data[i]);
+	}
+
 	return 0;
 }
--- red_attack/spoof/init_test.c
+++ red_attack/spoof/init_test.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "dns.h"
 #include "init.h"
 
-#define WCACHE_IP "1.1.1.41"
 #define BCACHE_IP "1.1.1.51"
+#define WCACHE_IP "1.1.1.41"
+#define TARGET_IP WCACHE_IP
 
 int main()
 {
 	struct sockaddr_in si = { 0 };
-	int si_init_ret = si_init(&si, WCACHE_IP, 53);
+	int si_init_ret = si_init(&si, TARGET_IP, 53);
+
 	if (si_init_ret == 0) {
 		fprintf(stderr, "si_init: invalid address\n");
 		exit(1);
@@ -18,10 +19,13 @@ int main()
 		perror("si_init");
 		exit(1);
 	}
+
 	int s = sock_init();
+
 	if (close(s)) {
 		perror("close");
 		exit(1);
 	}
+
 	return 0;
 }
