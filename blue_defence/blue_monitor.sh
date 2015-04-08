022e374daaa988d4757dbb206d4275f81a04a4ea
Ananth Mohan <ananthmo@usc.edu>
2015-04-08 14:02:30 -0700

    Fixed stuff

--- blue_defence/blue_monitor.sh
+++ blue_defence/blue_monitor.sh
@@ -6,11 +6,11 @@ else
   servers_file=$1
   ip_map_file=$2
   while true; do
-    sudo ./ip_map_creator.sh "${servers_file}" "${BCACHE_IP}" _b_"${ip_map_file}"
-    diff --unchanged-line-format= --old-line-format= --new-line-format='%L' "${ip_map_file}" _b_"${ip_map_file}" > _blue_diffs.txt
+    sudo ./ip_map_creator.sh "${servers_file}" "${BCACHE_IP}" _b_"${ip_map_file}" ignore
+    diff -w --unchanged-line-format= --old-line-format= --new-line-format='%L' "${ip_map_file}" _b_"${ip_map_file}" > _blue_diffs.txt
     while read line; do
-      set -- "${line}"
-      servername=$1
+      stringarr=($line)
+      servername=${stringarr[0]}
       unbound-control flush "${servername}"
       echo "Flushed ${servername}"
     done < _blue_diffs.txt
